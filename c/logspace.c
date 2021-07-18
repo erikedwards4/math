bfb61fd664dc2ce5e2bebecc052aa0926d45ee43
Erik Edwards <erik.edwards4@gmail.com>
2021-07-17 20:03:54 -0500

    v5

--- c/logspace.c
+++ c/logspace.c
@@ -17,11 +17,15 @@ int logspace_z (double *Y, const size_t N, const double a, const double b);
 
 int logspace_s (float *Y, const size_t N, const float a, const float b)
 {
-    const float stp = (b-a)/(N-1u);
-    float y = a;
+    if (N==0u) {}
+    else
+    {
+        const float stp = (b-a)/(float)(N-1u);
+        float y = a;
 
-    for (size_t n=0u; n<N-1u; ++n, ++Y, y+=stp) { *Y = powf(10.0f,y); }
-    *Y = powf(10.0f,b);
+        for (size_t n=0u; n<N-1u; ++n, ++Y, y+=stp) { *Y = powf(10.0f,y); }
+        *Y = powf(10.0f,b);
+    }
 
     return 0;
 }
@@ -29,11 +33,15 @@ int logspace_s (float *Y, const size_t N, const float a, const float b)
 
 int logspace_d (double *Y, const size_t N, const double a, const double b)
 {
-    const double stp = (b-a)/(N-1u);
-    double y = a;
+    if (N==0u) {}
+    else
+    {
+        const double stp = (b-a)/(double)(N-1u);
+        double y = a;
 
-    for (size_t n=0u; n<N-1u; ++n, ++Y, y+=stp) { *Y = pow(10.0,y); }
-    *Y = pow(10.0,b);
+        for (size_t n=0u; n<N-1u; ++n, ++Y, y+=stp) { *Y = pow(10.0,y); }
+        *Y = pow(10.0,b);
+    }
 
     return 0;
 }
@@ -41,11 +49,15 @@ int logspace_d (double *Y, const size_t N, const double a, const double b)
 
 int logspace_c (float *Y, const size_t N, const float a, const float b)
 {
-    const float stp = (b-a)/(N-1u);
-    float yr = a;
+    if (N==0u) {}
+    else
+    {
+        const float stp = (b-a)/(float)(N-1u);
+        float yr = a;
 
-    for (size_t n=0u; n<N-1u; ++n, ++Y, yr+=stp) { *Y = powf(10.0f,yr); *++Y = 0.0f; }
-    *Y = powf(10.0f,b); *++Y = 0.0f;
+        for (size_t n=0u; n<N-1u; ++n, ++Y, yr+=stp) { *Y = powf(10.0f,yr); *++Y = 0.0f; }
+        *Y = powf(10.0f,b); *++Y = 0.0f;
+    }
 
     return 0;
 }
@@ -53,11 +65,15 @@ int logspace_c (float *Y, const size_t N, const float a, const float b)
 
 int logspace_z (double *Y, const size_t N, const double a, const double b)
 {
-    const double stp = (b-a)/(N-1u);
-    double yr = a;
+    if (N==0u) {}
+    else
+    {
+        const double stp = (b-a)/(double)(N-1u);
+        double yr = a;
 
-    for (size_t n=0u; n<N-1u; ++n, ++Y, yr+=stp) { *Y = pow(10.0,yr); *++Y = 0.0; }
-    *Y = pow(10.0,b); *++Y = 0.0;
+        for (size_t n=0u; n<N-1u; ++n, ++Y, yr+=stp) { *Y = pow(10.0,yr); *++Y = 0.0; }
+        *Y = pow(10.0,b); *++Y = 0.0;
+    }
 
     return 0;
 }
